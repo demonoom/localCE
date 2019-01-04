ed2e2f88294e5cd9055adecb1f1942c717704c39
noom <noom0130@foxmail.com>
2019-01-04 14:11:40 +0800

    第一次提交

---
+++ main.js
@@ -0,0 +1,39 @@
+//项目入口文件
+const {app, BrowserWindow} = require('electron');
+const path = require('path');
+const url = require('url');
+
+app.on('ready', () => {
+    ant_createWin();
+});
+
+let win;
+
+function ant_createWin() {
+    win = new BrowserWindow({
+        width: 500,
+        height: 500,
+        title: '本地授课助手',
+        resizable: false,
+    });
+
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, './views/login.html'),
+        slashes: true,
+        protocol: 'file'
+    }));
+
+    win.webContents.openDevTools();
+
+    win.on('close', () => {
+        win = null;
+        app.quit();
+    });
+
+    win.on('ready-to-show', () => {
+        win.show();
+        win.focus();
+    });
+
+    win.setMenuBarVisibility(false);
+}
