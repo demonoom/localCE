6ec3846e96b16df476044d1c96f7912a4d7d0f66
noom <noom0130@foxmail.com>
2019-01-06 01:12:21 +0800

    截图完成1.0

--- renderProcess/capture.js
+++ renderProcess/capture.js
@@ -1,11 +1,8 @@
-const {
-    ipcRenderer, clipboard, nativeImage, remote,
-} = require('electron')
-
+const { ipcRenderer, clipboard, nativeImage, remote, desktopCapturer, screen } = require('electron')
+const Event = require('events')
 const fs = require('fs')
-const { getScreenSources } = require('../public/desktop-capturer')
-const { CaptureEditor } = require('../public/capture-editor')
-const { getCurrentScreen } = require('../public/utils')
+
+let { bounds: { width, height }, scaleFactor } = screen.getPrimaryDisplay()
 
 const $canvas = document.getElementById('js-canvas')
 const $bg = document.getElementById('js-bg')
@@ -20,22 +17,35 @@ const $btnReset = document.getElementById('js-tool-reset')
 const audio = new Audio()
 audio.src = '../assets/audio/capture.mp3'
 
-const currentScreen = getCurrentScreen()
+const ANCHORS = [
+    { row: 'x', col: 'y', cursor: 'nwse-resize' },
+    { row: '', col: 'y', cursor: 'ns-resize' },
+    { row: 'r', col: 'y', cursor: 'nesw-resize' },
 
-// 右键取消截屏
-document.body.addEventListener('mousedown', (e) => {
-    if (e.button === 2) {
-        window.close()
+    { row: 'x', col: '', cursor: 'ew-resize' },
+    { row: 'r', col: '', cursor: 'ew-resize' },
+
+    { row: 'x', col: 'b', cursor: 'nesw-resize' },
+    { row: '', col: 'b', cursor: 'ns-resize' },
+    { row: 'r', col: 'b', cursor: 'nwse-resize' },
+
+]
+
+console.time('capture')
+desktopCapturer.getSources({
+    types: ['screen'],
+    thumbnailSize: {
+        width: width * scaleFactor,
+        height: height * scaleFactor,
     }
-}, true)
+}, (error, sources) => {
+    console.timeEnd('capture')
+    let imgSrc = sources[0].thumbnail.toDataURL()
 
-// console.time('capture')
-getScreenSources({}, (imgSrc) => {
+    let capture = new Capture($canvas, imgSrc, scaleFactor)
+    $bg.style.backgroundImage = `url(${imgSrc})`
+    $bg.style.backgroundSize = `${width}px ${height}px`
 
-    // console.timeEnd('capture')
-
-
-    let capture = new CaptureEditor($canvas, $bg, imgSrc);
 
     let onDrag = (selectRect) => {
         $toolbar.style.display = 'none'
@@ -53,27 +63,13 @@ getScreenSources({}, (imgSrc) => {
 
     let onDragEnd = () => {
         if (capture.selectRect) {
-            ipcRenderer.send('capture-screen', {
-                type: 'select',
-                screenId: currentScreen.id,
-            })
-            const {
-                r, b,
-            } = capture.selectRect
+            const { x, r, b, y } = capture.selectRect
             $toolbar.style.display = 'flex'
             $toolbar.style.top = `${b + 15}px`
             $toolbar.style.right = `${window.screen.width - r}px`
         }
     }
     capture.on('end-dragging', onDragEnd)
-
-    ipcRenderer.on('capture-screen', (e, { type, screenId }) => {
-        if (type === 'select') {
-            if (screenId && screenId !== currentScreen.id) {
-                capture.disable()
-            }
-        }
-    })
 
     capture.on('reset', () => {
         $toolbar.style.display = 'none'
@@ -108,7 +104,7 @@ getScreenSources({}, (imgSrc) => {
             url,
         })
 
-    }
+    };
     $btnOk.addEventListener('click', selectCapture)
 
     $btnSave.addEventListener('click', () => {
@@ -118,12 +114,11 @@ getScreenSources({}, (imgSrc) => {
         remote.dialog.showSaveDialog({
             filters: [{
                 name: 'Images',
-                extensions: ['png', 'jpg', 'gif'],
-            }],
-        }, (path) => {
+                extensions: ['png', 'jpg', 'gif']
+            }]
+        }, function (path) {
             if (path) {
-                // eslint-disable-next-line no-buffer-constructor
-                fs.writeFile(path, new Buffer(url.replace('data:image/png;base64,', ''), 'base64'), () => {
+                fs.writeFile(path, new Buffer(url.replace('data:image/png;base64,', ''), 'base64'), function () {
                     ipcRenderer.send('capture-screen', {
                         type: 'complete',
                         url,
@@ -148,5 +143,325 @@ getScreenSources({}, (imgSrc) => {
     })
 })
 
+const CREATE_RECT = 1
+const MOVING_RECT = 2
+const RESIZE = 3
+
+class Capture extends Event {
+
+    constructor($canvas, imageSrc, scaleFactor) {
+        super()
+        this.$canvas = $canvas
+        this.imageSrc = imageSrc
+        this.scaleFactor = scaleFactor
+        this.ctx = $canvas.getContext('2d')
+
+        this.onMouseDown = this.onMouseDown.bind(this)
+        this.onMouseMove = this.onMouseMove.bind(this)
+        this.onMouseUp = this.onMouseUp.bind(this)
+
+        this.init().then(() => {
+            console.log('init')
+        })
+    }
+
+    async init() {
+        let canvas = document.createElement('canvas')
+        let ctx = canvas.getContext('2d')
+        let img = await new Promise(resolve => {
+            let img = new Image()
+            img.src = this.imageSrc
+            if (img.complete) {
+                resolve(img)
+            } else {
+                img.onload = () => resolve(img)
+            }
+        })
+
+        canvas.width = img.width
+        canvas.height = img.height
+        ctx.drawImage(img, 0, 0)
+        this.bgCtx = ctx
+
+        document.addEventListener('mousedown', this.onMouseDown)
+        document.addEventListener('mousemove', this.onMouseMove)
+        document.addEventListener('mouseup', this.onMouseUp)
+    }
+
+    onMouseDown(e) {
+        this.mouseDown = true
+        const { pageX, pageY } = e
+        if (this.selectRect) {
+            const { w, h, x, y, r, b } = this.selectRect
+            if (this.selectAnchorIndex !== -1) {
+                this.startPoint = {
+                    x: pageX,
+                    y: pageY,
+                    moved: false,
+                    selectRect: { w, h, x, y, r, b },
+                    rawRect: { w, h, x, y, r, b },
+                }
+                this.action = RESIZE
+                return
+            }
+            this.startPoint = {
+                x: e.pageX,
+                y: e.pageY,
+                moved: false,
+            }
+            if (pageX > x && pageX < r && pageY > y && pageY < b) {
+                this.action = MOVING_RECT
+                this.startDragRect = {
+                    x: pageX,
+                    y: pageY,
+                    selectRect: {
+                        x, y, w, h, r, b
+                    }
+                }
+            } else {
+                this.action = CREATE_RECT
+            }
+        } else {
+            this.action = CREATE_RECT
+            this.startPoint = {
+                x: e.pageX,
+                y: e.pageY,
+                moved: false,
+            }
+            e.stopPropagation()
+            e.preventDefault()
+        }
+    }
+
+    onMouseDrag(e) {
+        e.stopPropagation()
+        e.preventDefault()
+
+        const { pageX, pageY } = e
+        let startDragging
+        let selectRect = this.selectRect
+        if (!this.startPoint.moved) {
+            if (Math.abs(this.startPoint.x - pageX) > 10 || Math.abs(this.startPoint.y - pageY) > 10) {
+                this.startPoint.moved = true
+                startDragging = true
+            }
+        }
+        if (!this.startPoint.moved) {
+            return
+        }
+
+        if (this.action === MOVING_RECT) {
+            // 移动选区
+            if (startDragging) {
+                this.emit('start-dragging', selectRect)
+            }
+            this.emit('dragging', selectRect)
+            const { w, h, x, y, r, b } = selectRect
+            const { x: startX, y: startY } = this.startPoint
+            selectRect = this.selectRect = {
+                w,
+                h,
+                x: this.startDragRect.selectRect.x + pageX - startX,
+                y: this.startDragRect.selectRect.y + pageY - startY,
+                r: x + w,
+                b: y + h
+            }
+            this.drawRect()
+        } else if (this.action === RESIZE) {
+            this.emit('dragging', selectRect)
+            let { row, col } = ANCHORS[this.selectAnchorIndex]
+            if (row) {
+                this.startPoint.rawRect[row] = this.startPoint.selectRect[row] + pageX - this.startPoint.x
+                selectRect.x = this.startPoint.rawRect.x
+                selectRect.r = this.startPoint.rawRect.r
+                if (selectRect.x > selectRect.r) {
+                    let x = selectRect.r
+                    selectRect.r = selectRect.x
+                    selectRect.x = x
+                }
+                selectRect.w = selectRect.r - selectRect.x
+                this.startPoint.rawRect.w = selectRect.w
+            }
+            if (col) {
+                this.startPoint.rawRect[col] = this.startPoint.selectRect[col] + pageY - this.startPoint.y
+                selectRect.y = this.startPoint.rawRect.y
+                selectRect.b = this.startPoint.rawRect.b
+
+                if (selectRect.y > selectRect.b) {
+                    let y = selectRect.b
+                    selectRect.b = selectRect.y
+                    selectRect.y = y
+                }
+                selectRect.h = selectRect.b - selectRect.y
+                this.startPoint.rawRect.h = selectRect.h
+            }
+            this.drawRect()
+        } else {
+            // 生成选区
+            const { pageX, pageY } = e
+            let x, y, w, h, r, b
+            if (this.startPoint.x > pageX) {
+                x = pageX
+                r = this.startPoint.x
+            } else {
+                r = pageX
+                x = this.startPoint.x
+            }
+            if (this.startPoint.y > pageY) {
+                y = pageY
+                b = this.startPoint.y
+            } else {
+                b = pageY
+                y = this.startPoint.y
+            }
+            w = r - x
+            h = b - y
 
 
+            this.selectRect = {
+                x, y, w, h, r, b
+            }
+            selectRect = this.selectRect
+            if (startDragging) {
+                this.emit('start-dragging', selectRect)
+            }
+            this.emit('dragging', selectRect)
+            this.drawRect(x, y, w, h)
+        }
+
+
+    }
+
+    drawRect() {
+        if (!this.selectRect) {
+            this.$canvas.style.display = 'none'
+            return
+        }
+        const { x, y, w, h } = this.selectRect
+
+        const scaleFactor = this.scaleFactor
+        let margin = 7
+        let radius = 5
+        this.$canvas.style.left = `${x - margin}px`
+        this.$canvas.style.top = `${y - margin}px`
+        this.$canvas.style.width = `${w + margin * 2}px`
+        this.$canvas.style.height = `${h + margin * 2}px`
+        this.$canvas.style.display = 'block'
+        this.$canvas.width = (w + margin * 2) * scaleFactor
+        this.$canvas.height = (h + margin * 2) * scaleFactor
+
+        if (w && h) {
+            let imageData = this.bgCtx.getImageData(x * scaleFactor, y * scaleFactor, w * scaleFactor, h * scaleFactor)
+            this.ctx.putImageData(imageData, margin * scaleFactor, margin * scaleFactor)
+        }
+        this.ctx.fillStyle = '#ffffff'
+        this.ctx.strokeStyle = '#67bade'
+        this.ctx.lineWidth = 2 * this.scaleFactor
+
+        this.ctx.strokeRect(margin * scaleFactor, margin * scaleFactor, w * scaleFactor, h * scaleFactor)
+        this.drawAnchors(w, h, margin, scaleFactor, radius)
+    }
+
+    drawAnchors(w, h, margin, scaleFactor, radius) {
+        if (this.mouseDown && this.action === CREATE_RECT) {
+            this.anchors = null
+            return
+        }
+        this.ctx.beginPath()
+        let anchors = [
+            [0, 0],
+            [w * this.scaleFactor / 2, 0],
+            [w * this.scaleFactor, 0],
+
+            [0, h * this.scaleFactor / 2],
+            [w * this.scaleFactor, h * this.scaleFactor / 2],
+
+            [0, h * this.scaleFactor],
+            [w * this.scaleFactor / 2, h * this.scaleFactor],
+            [w * this.scaleFactor, h * this.scaleFactor],
+        ]
+        this.anchors = anchors.map(([x, y]) => [this.selectRect.x + x * 0.5, this.selectRect.y + y * 0.5])
+        anchors.forEach(([x, y], i) => {
+            this.ctx.arc(x + margin * scaleFactor, y + margin * scaleFactor, radius * scaleFactor, 0, 2 * Math.PI)
+            let next = anchors[(i + 1) % anchors.length]
+            this.ctx.moveTo(next[0] + margin * scaleFactor + radius * scaleFactor, next[1] + margin * scaleFactor)
+        })
+        this.ctx.closePath()
+        this.ctx.fill()
+        this.ctx.stroke()
+    }
+
+    onMouseMove(e) {
+        if (this.mouseDown) {
+            this.onMouseDrag(e)
+            return
+        }
+        this.selectAnchorIndex = -1
+        if (this.selectRect) {
+            const { pageX, pageY } = e
+            const { w, h, x, y, r, b } = this.selectRect
+            let selectAnchor, selectIndex = -1
+            if (this.anchors) {
+                console.log(pageX, pageY)
+                this.anchors.forEach(([x, y], i) => {
+                    console.log(x, y, Math.abs(pageX - x), Math.abs(pageY - y))
+                    if (Math.abs(pageX - x) <= 10 && Math.abs(pageY - y) <= 10) {
+                        selectAnchor = [x, y]
+                        selectIndex = i
+                    }
+                })
+            }
+            if (selectAnchor) {
+                this.selectAnchorIndex = selectIndex
+                document.body.style.cursor = ANCHORS[selectIndex].cursor
+                this.emit('moving')
+                return
+            }
+            if (pageX > x && pageX < r && pageY > y && pageY < b) {
+                document.body.style.cursor = 'move'
+            } else {
+                document.body.style.cursor = 'auto'
+            }
+            this.emit('moving')
+        }
+    }
+
+    onMouseUp(e) {
+        if (!this.mouseDown) {
+            return
+        }
+        this.mouseDown = false
+        e.stopPropagation()
+        e.preventDefault()
+        this.emit('mouse-up')
+        if (!this.startPoint.moved) {
+            this.emit('end-moving')
+            return
+        }
+        this.emit('end-dragging')
+        this.drawRect()
+        this.startPoint = null
+    }
+
+    getImageUrl() {
+        const { x, y, w, h } = this.selectRect
+        if (w && h) {
+            let imageData = this.bgCtx.getImageData(x * scaleFactor, y * scaleFactor, w * scaleFactor, h * scaleFactor)
+            let canvas = document.createElement('canvas')
+            let ctx = canvas.getContext('2d')
+            ctx.putImageData(imageData, 0, 0)
+            return canvas.toDataURL()
+        }
+        return ''
+    }
+
+    reset() {
+        this.anchors = null
+        this.startPoint = null
+        this.selectRect = null
+        this.startDragRect = null
+        this.selectAnchorIndex = -1
+        this.drawRect()
+        this.emit('reset')
+    }
+}
